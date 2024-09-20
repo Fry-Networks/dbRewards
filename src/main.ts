9d943d326968a275ad956e8c277ad9d9ab0b8db0
Yoyu <157239347+yoyumiracle@users.noreply.github.com>
2024-09-20 00:55:02 -0700

    complete reward test?

--- src/main.ts
+++ src/main.ts
@@ -52,7 +52,8 @@ const main = async () => {
 
     const allDevices = await DeviceModel.find({ is_registered: true }) as Device[];
     //const filtered = allDevices.filter((device) => device.reward_wallet)
-    const filtered = allDevices.filter((device) => device.miner_key.split('-')[0] == "CN");
+    // const filtered = allDevices.filter((device) => device.miner_key.split('-')[0] == "CN");
+    const filtered = allDevices;
     console.log(filtered);
     //console.log(await client.status().do());
     const account = mnemonicToSecretKey(process.env.MNEMONIC!);
@@ -74,90 +75,117 @@ const main = async () => {
                 console.log(`No reward wallet for miner ${device.miner_key}`);
                 continue;
             }
-            if (product.type === "hardware") {
+
+            // if (product.type === "hardware") {
 
 
 
-                const transactionsNeeded = 24;
-                //Calculate the amount of FRY to send for the devices that need transactions (POC)
-                const lastTransactions = await indexer.lookupAccountTransactions(address).limit(transactionsNeeded + 150).do();
-                //get all the transactions of the address that were done in the last 24 hours
-                const oneDayAgo = new Date(Date.now() - 24 * 60 * 60 * 1000);
+            //     const transactionsNeeded = 24;
+            //     //Calculate the amount of FRY to send for the devices that need transactions (POC)
+            //     const lastTransactions = await indexer.lookupAccountTransactions(address).limit(transactionsNeeded + 150).do();
+            //     //get all the transactions of the address that were done in the last 24 hours
+            //     const oneDayAgo = new Date(Date.now() - 24 * 60 * 60 * 1000);
 
-                const lastTransactionsInLast24Hours: Array<any> = lastTransactions.transactions.filter((transaction: Transaction) => {
-                    const transactionDate = new Date(transaction['round-time'] * 1000);
-                    if(transactionDate < oneDayAgo) return false;
-                    const isTheSender = transaction.sender === address;
-                    if(!isTheSender) return false;
-                    const isAmountZero = !transaction['asset-transfer-transaction'] || transaction['asset-transfer-transaction'].amount === 0;
-                    if(!isAmountZero) return false;
-                    const isFRY = transaction['asset-transfer-transaction'] && transaction['asset-transfer-transaction']['asset-id'] === config.asset_index;
-                    if(!isFRY) return false;
-                    const note = transaction.note;
-                    const decodeBase64 = Buffer.from(note, 'base64').toString('utf-8');
-                    console.log(decodeBase64);
-                    try {
-                    const decrypted = decryptWithPrivateKey(privateKeyPem, decodeBase64);
-                    console.log(decrypted);
-                    const isSameDevice = decrypted == device.miner_key
-                    return (isSameDevice);
-                    } catch (e) {
-                        console.log(e);
-                        return false;
-                    }
+            //     const lastTransactionsInLast24Hours: Array<any> = lastTransactions.transactions.filter((transaction: Transaction) => {
+            //         const transactionDate = new Date(transaction['round-time'] * 1000);
+            //         if(transactionDate < oneDayAgo) return false;
+            //         const isTheSender = transaction.sender === address;
+            //         if(!isTheSender) return false;
+            //         const isAmountZero = !transaction['asset-transfer-transaction'] || transaction['asset-transfer-transaction'].amount === 0;
+            //         if(!isAmountZero) return false;
+            //         const isFRY = transaction['asset-transfer-transaction'] && transaction['asset-transfer-transaction']['asset-id'] === config.asset_index;
+            //         if(!isFRY) return false;
+            //         const note = transaction.note;
+            //         const decodeBase64 = Buffer.from(note, 'base64').toString('utf-8');
+            //         console.log(decodeBase64);
+            //         try {
+            //         const decrypted = decryptWithPrivateKey(privateKeyPem, decodeBase64);
+            //         console.log(decrypted);
+            //         const isSameDevice = decrypted == device.miner_key
+            //         return (isSameDevice);
+            //         } catch (e) {
+            //             console.log(e);
+            //             return false;
+            //         }
                     
                     
-                });
+            //     });
      
-                //if there is at least 24 transactions in the last 24 hours, with 0 amount, then send the FRY
+            //     //if there is at least 24 transactions in the last 24 hours, with 0 amount, then send the FRY
 
-                let mult = 1;
-                if (lastTransactionsInLast24Hours.length >= transactionsNeeded) {
-                    mult = 1;
-                } else {
-                    mult = lastTransactionsInLast24Hours.length / transactionsNeeded;
-                }
+            //     let mult = 1;
+            //     if (lastTransactionsInLast24Hours.length >= transactionsNeeded) {
+            //         mult = 1;
+            //     } else {
+            //         mult = lastTransactionsInLast24Hours.length / transactionsNeeded;
+            //     }
 
-                const reward = (device.verified ? product?.reward.verified : product?.reward.unverified) ?? 0
-                amount = Math.floor(device.byod ? Math.round(reward * mult * 100) / 200 : Math.round(reward * mult * 100) / 100) //byod devices get half the reward
-                console.log(`amount for ${device.miner_key} is ${amount * globalMulitplier} -- ${lastTransactionsInLast24Hours.length} transactions in the last 24 hours}`)
-            } else {
-                amount = (device.verified ? product?.reward.verified : product?.reward.unverified) ?? 0
+            //     const reward = (device.verified ? product?.reward.verified : product?.reward.unverified) ?? 0
+            //     amount = Math.floor(device.byod ? Math.round(reward * mult * 100) / 200 : Math.round(reward * mult * 100) / 100) //byod devices get half the reward
+            //     console.log(`amount for ${device.miner_key} is ${amount * globalMulitplier} -- ${lastTransactionsInLast24Hours.length} transactions in the last 24 hours}`)
+            // } else 
+            if (device.verified)
+            {
+                amount = product?.reward.verified ?? 0
+                amount *= device.staked?.type === "one" ? 1.5 : 3; //Multiple reward amount following FIP-0007
                 console.log(`amount for ${device.miner_key} is ${amount * globalMulitplier}`)
+            } else {
+                amount = (product?.reward.unverified) ?? 0;
+                console.log(`amount for ${device.miner_key} is ${amount * globalMulitplier}`)
+            }
+
+            if (device.byod) { //if byod then reward must be in half
+                amount = Math.round(amount *  100) / 200;
             }
             //Calculate the total amount of FRY to send
             const amountToSend = (amount) * globalMulitplier * 1_000_000;
 
-
-
-
-
-            if (amountToSend > 0) {
-
-                if (!(await hasOptedInForAsset(address, config.asset_index))) {
-                    console.log(`Address ${address} has not opted in for asset ${config.asset_index}. Sending opt-in transaction.`);
-                    await optInForAsset(account, address, config.asset_index);
-                }
-
-                const note = enc.encode(device.miner_key.split('-')[0]);
-
-                const txn = makeAssetTransferTxnWithSuggestedParamsFromObject({
-                    from: account.addr,
-                    to: address,
-                    amount: amountToSend,
-                    assetIndex: config.asset_index,
-                    note: note,
-                    suggestedParams: params,
-                }
-                );
-                //convert the account sk object to Uint8Array
-                const signedTxn = txn.signTxn(account.sk);
-                const tx = (await client.sendRawTransaction(signedTxn).do());
-                console.log("Transaction : " + tx.txId);
-
-            } else {
+            if (amount <= 0) {
                 console.log('The miner: ' + device.miner_key + ' has no transactions in the last 24 hours');
+                continue;
             }
+
+            const oneDayAgo = new Date(Date.now() - 24 * 60 * 60 * 1000);
+
+            if (device.staked && (device.staked.reward_time > oneDayAgo))  {
+                console.log('The miner: ' + device.miner_key + ' is not reach reward time');
+                continue;
+            }
+
+            if (!(await hasOptedInForAsset(address, config.asset_index))) {
+                console.log(`Address ${address} has not opted in for asset ${config.asset_index}. Sending opt-in transaction.`);
+                await optInForAsset(account, address, config.asset_index);
+            }
+
+            const note = enc.encode(device.miner_key.split('-')[0]);
+
+            const txn = makeAssetTransferTxnWithSuggestedParamsFromObject({
+                from: account.addr,
+                to: address,
+                amount: amountToSend,
+                assetIndex: config.asset_index,
+                note: note,
+                suggestedParams: params,
+            }
+            );
+            //convert the account sk object to Uint8Array
+            const signedTxn = txn.signTxn(account.sk);
+            const tx = (await client.sendRawTransaction(signedTxn).do());
+            console.log("Transaction : " + tx.txId);
+            // adjust the rewarded time with delta 
+            const rewardedTime = Date.now();
+            const updateResult = await DeviceModel.updateOne({miner_key:device.miner_key, address:device.address}, {$set: {
+                stake: {
+                    rewarded_time: rewardedTime
+                }
+            }});
+            
+            if (updateResult.matchedCount > 0) {
+                console.log('The miner: ' + device.miner_key + ' rewarded time updated successfully');
+            } else {
+                console.log('No matching device found');
+            }
+            
         } catch (e) {
             console.log(e);
             console.log('Error for miner: ' + device.miner_key);
